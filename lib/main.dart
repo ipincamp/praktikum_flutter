19fefc3abfe55c435dc97e87b3aeffe43e617701
ipincamp <support@nur-arifin.my.id>
2024-09-17 11:40:47 +0700

    Separate pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:warung_bekicot/page/home.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,60 +17,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Warung Bekicot Magetan'),
-      ),
-      body: Container(
-        color: Colors.red.shade100,
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              ClipRRect(
-                borderRadius: BorderRadius.circular(10),
-                child: ClipOval(
-                  child: Image(image: AssetImage('bekicot.jpg')),
-                ),
-              ),
-              const SizedBox(height: 20),
-              const Text(
-                'Warung Bekicot',
-                style: TextStyle(
-                  fontSize: 26,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              const Text(
-                'Jl. Raya Magetan - Madiun',
-                style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                  color: Color.fromARGB(255, 70, 70, 70),
-                ),
-              ),
-              const Text(
-                'Telp. 628123456789',
-                style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                  color: Color.fromARGB(255, 70, 70, 70),
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
+      home: const HomePage(),
     );
   }
 }
